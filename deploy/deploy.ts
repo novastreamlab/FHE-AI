e43cccfceeaa8cce936180410f867475e67a6ad2
novastreamlab <TrottiJinny94@hotmail.com>
2025-10-18 23:42:38 +0800

    deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,17 +1,24 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const botAddress = process.env.BOT_ADDRESS ?? deployer;
+  const responsePlainAddress = process.env.RESPONSE_ADDRESS ?? deployer;
+
+  const deployedZamaAI = await deploy("ZamaAI", {
     from: deployer,
+    args: [botAddress, responsePlainAddress],
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`ZamaAI contract: `, deployedZamaAI.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_zama_ai"; // id required to prevent reexecution
+func.tags = ["ZamaAI"];
